0e795cdeb346776c9615b8d7b5dcef5ac209566a
Diwakar <diwakarpanchal.306@gmail.com>
2022-12-06 18:12:57 +0530

    remove console.log comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side JS loaded successfully');
-
 const form = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector("#message-1");
