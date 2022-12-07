7e20d5382595fde809c8f4f67afe4a09627d3d0e
Diwakar <diwakarpanchal.306@gmail.com>
2022-12-07 13:36:47 +0530

    port from env variable

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 3000
 const hbs = require('hbs')
 const geocode = require('./utils/geocode.js')
 const weatherstack = require('./utils/weatherstack.js')
@@ -122,6 +122,6 @@ app.get('*', (req, res) => {
     })
 })
 
- app.listen(3000, () => {
+ app.listen(port, () => {
     console.log("Server is up and running on port 3000.");
  })
